4786cf87d7078823b8c1cd1646a7b4e261a1361f
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-02-13 00:30:23 -0500

    Complete Arrays Lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+function appendCat(name) {
+    //const copyOfAppendCat = [...appendCat];
+    //cats.push(name);
+   // let arrayCopy = [...appendCat];
+    const copy = [...cats, name];
+    return copy;
+}
+function prependCat(name) {
+    const copy = [name, ...cats];
+    return copy;
+}
+function removeLastCat() {
+    const copy = cats.slice(0, cats.length-1);
+    return copy;
+
+}
+function removeFirstCat() {
+    const copy = cats.slice(1);
+    return copy;
+}

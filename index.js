e4c46d99bc9ae6d2871e0fdd110d76503ac99883
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-03-06 00:02:22 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution here!
-let cats = ["Milo", "Otis", "Garfield"];
+/*let cats = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendCat(name) {
     return cats.push(name);
 }
@@ -31,4 +31,43 @@ function removeLastCat() {
 function removeFirstCat() {
     const copy = cats.slice(1);
     return copy;
+} */
+
+let cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+
+}
+
+function destructivelyRemoveLastCat(name){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name){
+    return cats.shift();
+}
+
+function appendCat(name){
+    let copyCats = [...cats, name]
+    return copyCats;
+}
+
+function prependCat(name){
+    let copyCats = [name, ...cats]
+    return copyCats;
+}
+
+function removeLastCat(name){
+    let copyCats = cats.slice(0,-1)
+    return copyCats;
+}
+
+function removeFirstCat(name){
+    let copyCats = cats.slice(1);
+    return copyCats;
 }
